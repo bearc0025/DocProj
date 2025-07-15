f83bc9db8cb257f42357666206bdbd2d5cde9180
Bear Cahill <bcahill@etsy.com>
2025-07-15 12:10:19 -0500

    Initial Commit

---
+++ DocProj/DocProjApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocProjApp.swift
+//  DocProj
+//
+//  Created by Bear Cahill on 7/15/25.
+//
+
+import SwiftUI
+
+@main
+struct DocProjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DocProjTests/DocProjTests.swift
@@ -0,0 +1,17 @@
+//
+//  DocProjTests.swift
+//  DocProjTests
+//
+//  Created by Bear Cahill on 7/15/25.
+//
+
+import Testing
+@testable import DocProj
+
+struct DocProjTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
