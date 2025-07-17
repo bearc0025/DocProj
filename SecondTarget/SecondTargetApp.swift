70617ae1265d44a1ca95d457baea3e97fb6c1a95
Bear Cahill <bcahill@etsy.com>
2025-07-17 11:02:16 -0500

    second target

---
+++ SecondTarget/SecondTargetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecondTargetApp.swift
+//  SecondTarget
+//
+//  Created by Bear Cahill on 7/17/25.
+//
+
+import SwiftUI
+
+@main
+struct SecondTargetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SecondTargetTests/SecondTargetTests.swift
@@ -0,0 +1,17 @@
+//
+//  SecondTargetTests.swift
+//  SecondTargetTests
+//
+//  Created by Bear Cahill on 7/17/25.
+//
+
+import Testing
+@testable import SecondTarget
+
+struct SecondTargetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
